bec6d8a4e759c014c0df2a77d87952c7808ef645
Ahhyun, KANG <70644095+A-A27@users.noreply.github.com>
2022-10-12 23:44:27 +0900

    Rename 5장(2)_GL Modeling(GL_POINTS).cpp to Project2/5장(2)_GL Modeling(GL_POINTS).cpp

--- Project2/5장(2)_GL Modeling(GL_POINTS).cpp
+++ Project2/5장(2)_GL Modeling(GL_POINTS).cpp
@@ -1,4 +1,4 @@
-//4°³ÀÇ Á¡ ³ªÅ¸³»±â.
+//4ê°œì˜ ì  ë‚˜íƒ€ë‚´ê¸°.
 #include <GL/glut.h>
 
 void mydisplay() {
@@ -12,7 +12,7 @@ void mydisplay() {
 	glVertex2f(0.5, 0.5);
 	glVertex2f(-0.5, 0.5);
 	glEnd();
-	glFlush(); //»Ñ·ÁÁÖÁö ¾ÊÀ¸¸é È­¸é¿¡ ³ªÅ¸³ªÁö ¾Ê´Â´Ù.
+	glFlush(); //ë¿Œë ¤ì£¼ì§€ ì•Šìœ¼ë©´ í™”ë©´ì— ë‚˜íƒ€ë‚˜ì§€ ì•ŠëŠ”ë‹¤.
 }
 
 int main(int argc, char** argv) {
